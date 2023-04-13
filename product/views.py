b65c4a6a046ce8acf05e8b0c113e66486cac9240
yejin <yejin213527@gmail.com>
2023-04-13 13:44:29 +0900

    views ProductDetailView

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from product.models import Product
 
@@ -7,3 +7,7 @@ from product.models import Product
 class ProductListView(ListView):
     model = Product
     #'product_list.html',{'product_list':product.objects.all()}
+
+    class ProductDetailView(DetailView):
+        model = Product
+        #'Product_detail.html',{'Product' : Product.object.get(pk = pk)}
