e3828c96bf6f8d55abe96af479088a7249c25499
yejin <yejin213527@gmail.com>
2023-04-19 09:56:05 +0900

    models get_absolute_url():수정했을 때 성공하면, model의 detail로 가는 함수

--- product/models.py
+++ product/models.py
@@ -1,5 +1,7 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.shortcuts import resolve_url
+
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
@@ -7,3 +9,6 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}:{self.price}원'
+
+    def get_absolute_url(self): #모델 하나를 구하는 절대 주소
+        return resolve_url('product:detail',pk=self.id)
